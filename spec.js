7e72a81c9f1067454a0de309be4f0ead578fe2ed
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2018-01-16 10:57:47 +0100

    Spec 2

--- spec.js
+++ spec.js
@@ -1,6 +1,6 @@
 import Db from './dist'
 
-async function test (db) {
+async function testIndex (db) {
   // No index
   await db.put('key1', { diff: 1, foo: 2, common: 0 })
   await db.put('key2', { diff: 2, bar: 4, common: 0 })
@@ -61,10 +61,86 @@ async function test (db) {
   // rebuild index
   await db.reindex(['diff, common'])
   await db.find({ diff: 1, common: 0 }) // {key1: { diff: 1, foo: 2, common: 0 }}
+
+  // remove index
+  await db.removeIndex(['common'])
+  try {
+    await db.find({ common: 0 }) // Throws NotFoundError
+  } catch (error) {
+    if (!error.notFound) {
+      throw error
+    }
+  }
+
+  return
+}
+
+async function testUniqueIndex (db) {
+  // No index
+  await db.put('key1', { diff: 1, foo: 2, common: 0 })
+  await db.put('key2', { diff: 2, bar: 4, common: 0 })
+  await db.put('key3', { diff: 3, baz: 6, common: 0 })
+
+  try {
+    await db.get('key1') // { diff: 1, foo: 2, common: 0 }
+    await db.find({ diff: 1 }) // Throws NotFoundError
+  } catch (error) {
+    if (!error.notFound) {
+      throw error
+    }
+  }
+
+  // Index 'diff' as unique
+  await db.index(['diff'], {unique: true}) // or 'diff'
+  await db.put('key4', { diff: 4, qux: 8, common: 0 })
+
+  await db.find({ diff: 4 }) // {key4: { diff: 4, qux: 8, common: 0 }}
+  // AUTOMATICALLY Built on creation
+  await db.find({ diff: 1 }) // {key1: { diff: 1, foo: 2, common: 0 }}
+
+  try {
+    await db.put('anyUnexistingKey', { diff: 1, corge: 99, common: 0 }) // Throws UniqueIndexViolation
+  } catch (error) {
+    if (error.type !== 'UniqueIndexViolation') {
+      throw error
+    }
+    console.log(error.message) // Violation of Unique Index on 'diff'
+  }
+
+  // remove index
+  await db.removeIndex(['diff'])
+  try {
+    await db.find({ diff: 1 }) // Throws NotFoundError
+  } catch (error) {
+    if (!error.notFound) {
+      throw error
+    }
+  }
+
+  await db.put('anyUnexistingKey', { diff: 1, corge: 99, common: 0 })
+
+  // Try to create unique index on non-unique values:
+  try {
+    await db.index(['diff'], {unique: true}) // NotUniqueIndexKey
+  } catch (error) {
+    if (error.type !== 'NotUniqueIndexKey') {
+      throw error
+    }
+    console.log(error.message) // Cannot index 'diff' as unique, key has already multiple values
+  }
+
+  return
+}
+
+async function test (db) {
+  await testIndex(db)
+  await testUniqueIndex(db)
 }
 
 const db = new Db('./testdb', {
   keyEncoding: 'json',
   valueEncoding: 'json',
 })
+
+
 test(db)
